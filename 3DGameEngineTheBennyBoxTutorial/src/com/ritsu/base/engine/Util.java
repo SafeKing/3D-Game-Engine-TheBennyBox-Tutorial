4e0eeb3ad8b198215f0f93340e1854fe72509345
Ritsu <ritsu.chan.95@gmail.com>
2014-12-11 16:04:48 +0100

    #15 Mesh Faces

--- 3DGameEngineTheBennyBoxTutorial/src/com/ritsu/base/engine/Util.java
+++ 3DGameEngineTheBennyBoxTutorial/src/com/ritsu/base/engine/Util.java
@@ -1,6 +1,7 @@
 package com.ritsu.base.engine;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 
 import org.lwjgl.BufferUtils;
 
@@ -11,6 +12,18 @@ public class Util {
 
 	public static FloatBuffer createFloatBuffer(int size) {
 		return BufferUtils.createFloatBuffer(size);
+	}
+
+	public static IntBuffer createIntBuffer(int size) {
+		return BufferUtils.createIntBuffer(size);
+	}
+
+	public static IntBuffer createFlippedBuffer(int... values) {
+		IntBuffer buffer = createIntBuffer(values.length);
+		buffer.put(values);
+		buffer.flip();
+
+		return buffer;
 	}
 
 	public static FloatBuffer createFlippedBuffer(Vertex[] vertices) {
