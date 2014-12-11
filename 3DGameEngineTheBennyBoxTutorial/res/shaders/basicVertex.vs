22d71972b7458ece10ee416aab3d3d8aa17d0da5
Ritsu <ritsu.chan.95@gmail.com>
2014-12-11 13:36:37 +0100

    #12 Translation

--- 3DGameEngineTheBennyBoxTutorial/res/shaders/basicVertex.vs
+++ 3DGameEngineTheBennyBoxTutorial/res/shaders/basicVertex.vs
@@ -4,9 +4,9 @@ layout (location = 0) in vec3 position;
 
 out vec4 colour;
 
-uniform float uniformFloat;
+uniform mat4 transform;
 
 void main() {
-	colour = vec4(clamp(position, 0.0, uniformFloat), 1.0);
-	gl_Position = vec4(position, 1.0);
+	colour = vec4(clamp(position, 0.0, 1.0), 1.0);
+	gl_Position = transform * vec4(position, 1.0);
 	}
--- 3DGameEngineTheBennyBoxTutorial/src/com/ritsu/base/engine/Game.java
+++ 3DGameEngineTheBennyBoxTutorial/src/com/ritsu/base/engine/Game.java
@@ -11,6 +11,7 @@ public class Game {
 
 	private Mesh mesh;
 	private Shader shader;
+	private Transform transform;
 
 	public Game() {
 		mesh = new Mesh();
@@ -20,11 +21,13 @@ public class Game {
 
 		mesh.addVertices(data);
 
+		transform = new Transform();
+
 		shader.addVertexShader(ResourceLoader.loadShader("basicVertex.vs"));
 		shader.addFragmentShader(ResourceLoader.loadShader("basicFragment.fs"));
 		shader.compileShader();
 
-		shader.addUniform("uniformFloat");
+		shader.addUniform("transform");
 	}
 
 	public void input() {
@@ -47,11 +50,12 @@ public class Game {
 	public void update() {
 		temp += Time.getDelta();
 
-		shader.setUniformf("uniformFloat", (float) Math.abs(Math.sin(temp)));
+		transform.setTranslation((float) Math.sin(temp), 0, 0);
 	}
 
 	public void render() {
 		shader.bind();
+		shader.setUniform("transform", transform.getTransformation());
 		mesh.draw();
 	}
 
